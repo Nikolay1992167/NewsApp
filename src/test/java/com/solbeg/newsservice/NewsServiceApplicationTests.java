70847401a5e87917c7c3b09b59d27b3653a6b460
Nikolay1992167 <wulik1081@mail.ru>
2024-03-22 09:59:25 +0300

    init commit

---
+++ src/test/java/com/solbeg/newsservice/NewsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solbeg.newsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
